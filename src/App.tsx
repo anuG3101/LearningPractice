ff8f7c7db734c2a0583528be54fd2dbf2d3662a4
anuG3101 <anu.guptha@siemens.com>
2025-06-03 14:37:40 +0530

    Updates in progress.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<XRDisplay />} />
-            <Route path="/LearningPractice/XrDisplay" element={<XRDisplay />} />
+            <Route path="/LearningPractice" element={<XRDisplay />} />
           </Routes>
         </Router>
       </section>
